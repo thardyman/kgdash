e416e0d6f0165de139e058ae16589053b2905a58
Tim Hardyman <thardyman@gmail.com>
2016-10-19 16:57:44 +0100

    Replace drawRbox with drawBox

--- bar_gauge.ino
+++ bar_gauge.ino
@@ -6,16 +6,20 @@ void bar_gauge(int offsetX, int offsetY, int barCount, int flip, float inputVal)
   int i = 0;
   int barHeight = 6;
   int barBottomMargin = 3;
-  int barWidth = 12;
+  int barWidth[] = {14, 12, 10, 9, 8, 8, 7 };
   int barRadius = 2;
-
+  int barOffsetX = offsetX;
   float threshold[] = {0.84, 0.7, 0.56, 0.42, 0.28, 0.14};
   do{
+    if(flip){
+      barOffsetX = offsetX - barWidth[i];
+    }
     if(inputVal < threshold[i]){
-      u8g.drawRFrame(offsetX, offsetY + i * (barHeight + barBottomMargin), barWidth, barHeight, barRadius);
+      u8g.drawRFrame(barOffsetX, offsetY + i * (barHeight + barBottomMargin), barWidth[i], barHeight, barRadius);
     }
     else {
-      u8g.drawRBox(offsetX, offsetY + i * (barHeight + barBottomMargin), barWidth, barHeight, barRadius);
+      // drawRbox doesn't seem to work correctly, so we'll use a square box :(
+      u8g.drawBox(barOffsetX, offsetY + i * (barHeight + barBottomMargin), barWidth[i], barHeight);
     }
     i++;
 
--- dashboard_1.ino
+++ dashboard_1.ino
@@ -32,8 +32,8 @@ void dashboard_1_progloop() {
 
 // draw loop is should contain all the code to display the screen
 void dashboard_1_drawloop() {
-  bar_gauge(2, 2, 7, 0, fuel / 100);
-  bar_gauge(110, 2, 7, 1, temperature / 40);  // really this should be coolant!
+  bar_gauge(0, 2, 7, 0, fuel / 100);
+  bar_gauge(128, 2, 7, 1, temperature / 40);  // really this should be coolant!
   u8g.setPrintPos(20, 20);
   u8g.print("Temp: "); u8g.print(temperature); u8g.print(" *c");
 }
